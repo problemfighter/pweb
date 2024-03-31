bdb6f10b04c577f836672a8bee3024fa247ab055
Touhid Mia <touhid@banglafighter.com>
2024-03-31 20:55:27 +0600

    Debug Push

--- pweb/system12/pweb_base.py
+++ pweb/system12/pweb_base.py
@@ -8,7 +8,8 @@ class PWebBase(Flask):
     def is_app_loaded(self):
         print("?????????????????????")
         print(os.environ)
-        print(f"IS_WSGI {os.environ.get("IS_WSGI")}")
+        IS_WSGI = os.environ.get("IS_WSGI")
+        print(f"IS_WSGI {IS_WSGI}")
         if not self.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true" or self._is_run_on_wsgi:
             return True
         return False
