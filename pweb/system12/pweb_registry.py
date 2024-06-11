29dc52cafaf589dd545adbeb98c351e53ce25376
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-06-11 17:16:25 +0600

    Added new keys

--- pweb/system12/pweb_registry.py
+++ pweb/system12/pweb_registry.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pweb.system12.pweb_base import PWebBase
 from pweb.system12.pweb_interfaces import PWebModuleDetails
 from pweb.system12.pweb_app_config import PWebAppConfig
 
@@ -6,3 +7,4 @@ from pweb.system12.pweb_app_config import PWebAppConfig
 class PWebRegistry:
     config: PWebAppConfig = PWebAppConfig()
     registerModules: Dict[str, PWebModuleDetails] = {}
+    pweb_app: PWebBase = None
